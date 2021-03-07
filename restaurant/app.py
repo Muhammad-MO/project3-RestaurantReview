581633c609169da5c42dcd8a0f80bc3c5137d7c8
Muhammad-MO <muhdmahmood@gmail.com>
2021-03-07 13:42:30 +0000

    created restauant list display in cards and searc button

--- restaurant/app.py
+++ restaurant/app.py
@@ -11,9 +11,23 @@ load_dotenv()
 app = Flask(__name__)
 
 MONGO_URI = os.environ.get('MONGO_URI')
-print(MONGO_URI)
+DB_NAME = 'Restaurant'
+
+client = pymongo.MongoClient(MONGO_URI)
+db = client[DB_NAME]
 
 
-if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"), port=int(
-        os.environ.get("PORT")), debug=True)
+@app.route('/')
+def show_listings():
+    listings = db.restaurantname.find({}, {
+        'name': 1,
+        'cuisine': 1,
+        'ratings': 1
+    }).limit(15)
+
+    return render_template('restaurants.template.html', listings=listings)
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'), port=int(
+        os.environ.get('PORT')), debug=True)
