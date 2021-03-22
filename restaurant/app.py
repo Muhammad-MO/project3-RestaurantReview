6477a504bbdff85e07aa0708c61e685ffc7efb87
Muhammad-MO <muhdmahmood@gmail.com>
2021-03-22 16:23:42 +0000

    added create function

--- restaurant/app.py
+++ restaurant/app.py
@@ -17,33 +17,67 @@ client = pymongo.MongoClient(MONGO_URI)
 db = client[DB_NAME]
 
 
-@app.route('/')
+@app.route('/restaurants')
 def show_listings():
 
-    country = request.args.get('country')
-  
-   
+    flavour = request.args.get('flavour')
 
     criteria = {}
 
-    if country:
-        criteria['cuisine.country'] = country
+    if flavour:
+        criteria['cuisine.flavour'] = flavour
 
     listings = db.restaurantname.find(criteria, {
         'name': 1,
         'healthgrade': 1,
         'cuisine': 1,
+        'ratings': 1,
         'location': 1,
         'images': 1,
         'price': 1,
         'reviews': 1
 
-    }).limit(15)
+    }).limit(100)
 
-    return render_template('restaurants.template.html', listings=listings,
+    return render_template('all_restaurants.template.html', listings=listings,
                            fullpath=request.full_path)
 
 
+@app.route('/create')
+def show_create_restaurant():
+    return render_template('create_restaurant.template.html')
+
+
+@app.route('/create', methods=["POST"])
+def process_create_restaurant():
+    name = request.form.get('name')
+    flavour = request.form.get('flavour')
+    ratings = request.form.get('ratings')
+    healthgrade = request.form.get('healthgrade')
+    Address = request.form.get('Address')
+    Country = request.form.get('Country')
+    price = request.form.get('price')
+    reviews = request.form.get('reviews')
+
+    db.restaurantname.insert_one({
+        "name": name,
+        "cuisine": {
+            "flavour": flavour,
+        },
+        "ratings": ratings,
+        "location": {
+            "Address": Address,
+            "Country": Country
+        },
+        "price": price,
+        "reviews": reviews
+    })
+
+    return redirect(url_for('show_listings'))
+
+
+
+
+
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'), port=int(
-        os.environ.get('PORT')), debug=True)
+    app.run(host=os.environ.get('IP'), port=os.environ.get('PORT'), debug=True)
